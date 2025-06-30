f845501ae2dc770a572c2a10bfff8b408eb8758c
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-06-30 16:50:08 +0530

    Allow definition of a custom shell.
    This also allows the user to use strings in their commands.

--- addons/startup_runner/cmd_runner.gd
+++ addons/startup_runner/cmd_runner.gd
@@ -4,19 +4,27 @@ extends EditorPlugin
 # CHANGE THIS IF YOU WANT TO MOVE THE ADDON TO SOMEPLACE ELSE
 const commands_file_path:String = "res://addons/startup_runner/command.txt"
 
+# Get these from the command.txt file.
+var shell_name:String = "sh"
+var shell_execute_command:String = "-c"
+
 func _enter_tree() -> void: #Also runs on editor start.
 	if not FileAccess.file_exists(commands_file_path): #Intercept the error with our own.
 		push_error("STARTUP COMMAND RUNNER: Command file not found at %s. Was it moved or deleted?"%commands_file_path)
 	else:
 		# Get the contents of the file as a text and then split it by the line.
 		var commands = FileAccess.open(commands_file_path,FileAccess.READ).get_as_text().split("\n")
-		
-		for command in commands:
+
+		# Line 0 is the shell name, Line 1 is the execute argument.
+		# The operator makes sure we don't set the shell name to empty.
+		shell_name = commands[0] if commands[0] != "" else shell_name
+		shell_execute_command = commands[1] if commands[1] != "" else shell_execute_command
+
+		for command in commands.slice(2):
 			if not command.strip_edges() == "": #Don't execute a blank line on accident.
-				var command_name:String = command.split(" ")[0]
-				var command_arguments:PackedStringArray = command.split(" ").slice(1)
-				OS.execute_with_pipe(command_name,command_arguments)
-	
+				OS.execute_with_pipe(shell_name,[shell_execute_command, command],false)
+				print("%s %s %s"%[shell_name,shell_execute_command,command])
+
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
